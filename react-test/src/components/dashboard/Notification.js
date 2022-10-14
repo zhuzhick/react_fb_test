193f831211f79f71f9a9bd4344801c56f1665e99
zhuzhick <a.holtceva@gmail.com>
2022-10-14 14:26:05 +0300

    App Created

---
+++ react-test/src/components/dashboard/Notification.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notifications = () => {
+	return (
+		<div>
+			<p>Notications</p>
+		</div>
+	);
+};
+
+export default Notifications;
---
+++ react-test/src/components/projects/ProjectList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ProjectList = () => {
+	return (
+		<div className="project-list section">
+			<div className="card z-depth-0 project-summary">
+				<div className="card-content grey-text text-darken-3">
+					<span className="card-title">Project Title</span>
+					<p>Posted by ZhuZHa</p>
+					<p className="grey-text">14th October, 2022</p>
+				</div>
+			</div>
+			<div className="card z-depth-0 project-summary">
+				<div className="card-content grey-text text-darken-3">
+					<span className="card-title">Project Title</span>
+					<p>Posted by ZhuZHa</p>
+					<p className="grey-text">14th October, 2022</p>
+				</div>
+			</div>
+			<div className="card z-depth-0 project-summary">
+				<div className="card-content grey-text text-darken-3">
+					<span className="card-title">Project Title</span>
+					<p>Posted by ZhuZHa</p>
+					<p className="grey-text">14th October, 2022</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProjectList;
